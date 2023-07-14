1aa3cf5763b2bbb6721fd0959e80199733560ab4
Noah Beidelman <nbeidelman9@gmail.com>
2023-07-14 02:09:54 -0700

    add chrome, reset elements for scrolling,

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
+const chrome = require('selenium-webdriver/chrome');
 
 //const username = "";
 //const password = "";
@@ -12,24 +13,37 @@ const password = ""
 //const username = ""
 //const password = "23"
 
-const geckoDriverPath = 'C:/Windows/geckodriver.exe';
+const geckoDriverPath = 'C:/Users/tromb/webDev/instagram-scraping/geckodriver-v0.33.0-win64/geckodriver.exe';
+const chromeDriverPath = 'C:/Users/tromb/webDev/instagram-scraping/chromedriver_win32/chromedriver.exe';
+
 const firefoxOptions = new firefox.Options();
+const chromeOptions = new chrome.Options();
+// Set the path to the Firefox binary (optional)
+// firefoxOptions.setBinary('path/to/firefox');
+
+// Set the path to the Chrome binary (optional)
+// chromeOptions.setBinary('path/to/chrome');
 
 // Uncomment the following line if you want to run Firefox in headless mode
 // firefoxOptions.headless();
 
-// Set the path to the Firefox binary (optional)
-// firefoxOptions.setBinary('path/to/firefox');
+// Uncomment the following line if you want to run Chrome in headless mode
+// chromeOptions.headless();
+
 const followersArr = [];
 const followingArr = [];
 let follower = {};
 
 (async () => {
   const driver = await new Builder()
-    .forBrowser('firefox')
-    .setFirefoxOptions(firefoxOptions)
-    .setFirefoxService(new firefox.ServiceBuilder(geckoDriverPath))
-    .build();
+  //.forBrowser('firefox')
+  // Use the following line for Chrome browser
+  .forBrowser('chrome')
+  .setFirefoxOptions(firefoxOptions)
+  .setChromeOptions(chromeOptions)
+  .setFirefoxService(new firefox.ServiceBuilder(geckoDriverPath))
+  .setChromeService(new chrome.ServiceBuilder(chromeDriverPath))
+  .build();
 
   try {
     await driver.get("https://www.instagram.com");
@@ -43,8 +57,9 @@ let follower = {};
     const submitButton = await driver.wait(until.elementLocated(By.css("[type='submit']")), 10000, "Could not find Log in button");
     await driver.sleep(2000);
     await submitButton.click();
-    await driver.sleep(2000);
+    await driver.sleep(5000);
 
+    //need to wait until user is authenticated
     //navigate to profile page
     await driver.get("https://www.instagram.com/" + username + "/");
 
@@ -62,13 +77,13 @@ let follower = {};
     await driver.sleep(6000);
 
     //initialize elements
-    const divContainer = await driver.findElement(By.css('div._aano'));
-    const topContainer = await driver.findElement(By.css('div._aano :first-child'));
+    let divContainer = await driver.findElement(By.css('div._aano'));
+    let topContainer = await driver.findElement(By.css('div._aano :first-child'));
     let elements = await topContainer.findElements(By.css('span.x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft'));
 
     const scrollDiv = async (numUsers) => {
       try {
-        if (elements.length >= numUsers)
+        if ((elements.length)/2 >= numUsers)
           return;
         await driver.executeScript("arguments[0].scrollBy(0, 200)", divContainer);
         elements = await topContainer.findElements(By.css('span.x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft'));
@@ -83,6 +98,7 @@ let follower = {};
     await scrollDiv(numFollowers);
 
     //add all followers to followers array
+    //should maybe return array instead
     const pushToUserArray = async (array) => {
       for (let i = 0; i < elements.length; i += 2) {
         const usernameElem = await elements[i];
@@ -109,19 +125,20 @@ let follower = {};
     await pushToUserArray(followersArr);
 
     //close followers window
-    let closeButton = await findElement(By.css(("[aria-label='Close']")))
+    let closeButton = await driver.findElement(By.css("button._abl-"))
     await closeButton.click();
     await driver.sleep(2000);
 
     //open following window
     await numbers[2].click();
     await driver.sleep(6000);
-
-    //maybe need to go back to insta
-    //also click following button, don't go to link
     
     //reset the elements for followingArr
-    elements = await driver.wait(until.elementLocated(By.css('span.x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft'), 10000, "didn't find element array for following"));
+    divContainer = await driver.findElement(By.css('div._aano'));
+    topContainer = await driver.findElement(By.css('div._aano :first-child'));
+    await driver.wait(until.elementLocated(By.css('span.x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft'), 10000, "didn't find element array for following"))
+    elements = await driver.findElements(By.css('span.x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft'));
+    elements.splice(0, 10);
 
     // scroll down to reveal all following
     await scrollDiv(numFollowing);
@@ -129,7 +146,6 @@ let follower = {};
     //push following to array
     await pushToUserArray(followingArr)
     await driver.sleep(2000);
-
   } 
   finally {
     console.log("followers:\n" + JSON.stringify(followersArr, null, 2));

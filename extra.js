4c63f010530fda083bc0ceb99111db8972f588b8
Noah Beidelman <nbeidelman9@gmail.com>
2023-07-18 21:02:46 -0700

    remove unused files

--- extra.js
+++
@@ -1,8 +0,0 @@
-//for save login info popup
-    /* don't need this
-    const notNowButton = await driver.wait(until.elementLocated(By.xpath("//div[contains(text(),'Not Now')]")), 10000, "Could not find not now button");
-    await notNowButton.click();
-    await driver.sleep(3000);
-    const notNowButton2 = await driver.wait(until.elementLocated(By.xpath("//button[contains(text(),'Not Now')]")), 10000, "Could not find not now button");
-    await notNowButton2.click();
-    */

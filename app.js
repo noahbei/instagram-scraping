1c836f700ae446ceb90e39ad01d43a4d25773830
Noah Beidelman <nbeidelman9@gmail.com>
2023-07-31 18:04:07 -0700

    add link to download output

--- app.js
+++ app.js
@@ -15,10 +15,52 @@ app.post("/", async (req, res) => {
     let password = req.body.password;
     // -- autenticate user --
     await scrape.scrapeData(username, password);
-    let output = username + " " + password;
-    res.send(output);
+
+    await compressFiles([
+        "following.json",
+        "followers.json",
+        "overlap.json",
+        "onlyInFollowing.json",
+        "onlyInFollowers.json"
+    ], "output.zip");
+
+    res.write("<a style='display: block' href='/download/output.zip' download>Download all</a>");
+    res.write("<a style='display: block' href='/download/following.json' download>Download following</a>");
+    res.write("<a style='display: block' href='/download/followers.json' download>Download followers</a>");
+    res.write("<a style='display: block' href='/download/overlap.json' download>Download overlap</a>");
+    res.write("<a style='display: block' href='/download/onlyInFollowing.json' download>Download onlyInFollowing</a>");
+    res.write("<a style='display: block' href='/download/onlyInFollowers.json' download>Download onlyInFollowers</a>");
+    res.send();
 })
+
+app.get('/download/:filename', function(req, res){
+    const file = __dirname + "/output/" + req.params.filename;
+    res.download(file);
+  });
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 })
+
+async function compressFiles(fileList, zipFileName) {
+    const archiver = require("archiver");
+    const outputFilePath = __dirname + "/output/" + zipFileName;
+
+    const output = fs.createWriteStream(outputFilePath);
+    const archive = archiver("zip", {
+        zlib: { level: 9 }
+    });
+
+    output.on("close", () => {
+        console.log(archive.pointer() + " total bytes");
+    });
+
+    archive.pipe(output);
+
+    for (const file of fileList) {
+        const filePath = __dirname + "/output/" + file;
+        archive.append(fs.createReadStream(filePath), { name: file });
+    }
+
+    archive.finalize();
+}
